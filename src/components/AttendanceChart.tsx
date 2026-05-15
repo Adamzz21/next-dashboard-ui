97653f5780fd9eaec492ea32dfe05f0ed3b4cba9
imad <roulaimad7@gmail.com>
2026-05-15 19:46:25 +0100

    adding Students and Attendance charts

---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,113 @@
+"use client";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import { RechartsDevtools } from "@recharts/devtools";
+import Image from "next/image";
+
+const data = [
+  {
+    name: "Sat",
+    Present: "70",
+    Absent: "20",
+  },
+  {
+    name: "Sun",
+    Present: "80",
+    Absent: "50",
+  },
+  {
+    name: "Mon",
+    Present: "60",
+    Absent: "10",
+  },
+  {
+    name: "Tue",
+    Present: "80",
+    Absent: "33",
+  },
+  {
+    name: "Wed",
+    Present: "80",
+    Absent: "80",
+  },
+  {
+    name: "Thu",
+    Present: "70",
+    Absent: "55",
+  },
+  {
+    name: "Fri",
+    Present: "20",
+    Absent: "10",
+  },
+];
+
+const AttendanceChart = () => {
+  return (
+    <div className="bg-white rounded-lg p-4 h-full">
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Attendance</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20} />
+      </div>
+      <BarChart
+        style={{
+          height: "100%",
+          width: "100%",
+          maxWidth: "700px",
+          maxHeight: "80vh",
+          aspectRatio: 1.618,
+        }}
+        height={300}
+        width={500}
+        responsive
+        data={data}
+        barSize={40}
+      >
+        <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#ddd" />
+        <XAxis
+          dataKey="name"
+          axisLine={false}
+          tick={{ fill: "#d1d5db" }}
+          tickLine={false}
+        />
+        <YAxis
+          width="auto"
+          axisLine={false}
+          tick={{ fill: "#d1d5db" }}
+          tickLine={false}
+        />
+        <Tooltip
+          contentStyle={{ borderRadius: "10px", borderColor: "lightgray" }}
+        />
+        <Legend
+          align="left"
+          verticalAlign="top"
+          wrapperStyle={{ paddingTop: "20px", paddingBottom: "40px" }}
+        />
+        <Bar
+          dataKey="Present"
+          fill="#FAE27C"
+          activeBar={{ fill: "pink", stroke: "blue" }}
+          radius={[20, 20, 0, 0]}
+          legendType="circle"
+        />
+        <Bar
+          dataKey="Absent"
+          fill="#C3EBFA"
+          activeBar={{ fill: "gold", stroke: "purple" }}
+          radius={[20, 20, 0, 0]}
+          legendType="circle"
+        />
+        <RechartsDevtools />
+      </BarChart>
+    </div>
+  );
+};
+export default AttendanceChart;
--- src/components/CountCharts.tsx
+++ src/components/CountCharts.tsx
@@ -1,9 +1,90 @@
+"use client";
+import { RadialBarChart, RadialBar, Legend, Tooltip } from "recharts";
+import { RechartsDevtools } from "@recharts/devtools";
+import Image from "next/image";
+
+// #region Sample data
+const data = [
+  {
+    name: "Total",
+    count: 103,
+    fill: "#ffffff",
+  },
+  {
+    name: "Girls",
+    count: 50,
+    fill: "#FAE27C",
+  },
+  {
+    name: "Boys",
+    count: 53,
+    fill: "#C3EBFA",
+  },
+];
+
+// #endregion
+const style = {
+  top: "50%",
+  right: 0,
+  transform: "translate(0, -50%)",
+  lineHeight: "24px",
+};
 
 const CountCharts = () => {
   return (
-    <div className=''>CountCharts</div>
-   
-  )
-}
+    <div className="bg-white rounded-xl w-full h-full p-4">
+      {/* //TITLE --------------------------------------------*/}
+      <div className="flex justify-between items-center">
+        <h1 className="text-lg font-semibold">Students</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20} />
+      </div>
+      {/* CHART -------------------------------------------- */}
+      <div className=" flex relative w-full h-[75%]">
+        <RadialBarChart
+          style={{
+            width: "100%",
+            maxWidth: "700px",
+            maxHeight: "80vh",
+            aspectRatio: 1.618,
+          }}
+          innerRadius="40%"
+          outerRadius="100%"
+          responsive
+          cx="50%"
+          cy="50%"
+          barSize={32}
+          data={data}
+        >
+          <RadialBar background dataKey="count" />
+          <Tooltip />
+          <RechartsDevtools />
+        </RadialBarChart>
+        <Image
+          src="/maleFemale.png"
+          alt=""
+          width={50}
+          height={50}
+          className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+      </div>
+      {/* BOTTOM  --------------------------------------------*/}
+      <div className="flex justify-center gap-16">
+        {/* ------------------ */}
+        <div className="flex flex-col gap-2">
+          <div className="w-5 h-5 bg-lamaSky rounded-full" />
+          <h1 className="font-bold">1,234</h1>
+          <h2 className="text-xs text-gray-300">Boys(55%)</h2>
+        </div>
+        {/* ------------------ */}
+        <div className="flex flex-col gap-2">
+          <div className="w-5 h-5 bg-yellow-300 rounded-full" />
+          <h1 className="font-bold">1,234</h1>
+          <h2 className="text-xs text-gray-300">Girls(45%)</h2>
+        </div>
+        {/* ------------------ */}
+      </div>
+    </div>
+  );
+};
 
-export default CountCharts
+export default CountCharts;
