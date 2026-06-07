34fe853e23e97a15beb6cd15854abef4018d66d2
adem_djellit <ademdjellit1@gmail.com>
2026-06-07 10:54:27 +0100

    finished teachers list

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,26 @@
-const Table = () => {
-  return <table className="w-full mt-4"></table>;
+const Table = ({
+  columns,
+  renderRow,
+  data,
+}: {
+  columns: { header: string; accesor: string; className?: string }[];
+  renderRow: (item: any) => React.ReactNode;
+  data: any[];
+}) => {
+  return (
+    <table className="w-full mt-4">
+      <thead>
+        <tr className="text-left text-gray-500 text-xs">
+          {columns.map((col) => (
+            <th key={col.accesor} className={col.className}>
+              {col.header}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>{data.map((item) => renderRow(item))}</tbody>
+    </table>
+  );
 };
 
 export default Table;
